d458aefa2fa268e3fc72fe850b812f2a50358bda
Haoxiang Yang <yanghaox@oregonstate.edu>
2018-05-02 12:22:32 -0700

    deal with data

--- Logistic_regression_01.py
+++ Logistic_regression_01.py
@@ -10,6 +10,24 @@ from sklearn import preprocessing
 
 data = pd.read_csv('banking.csv', header=0)
 data = data.dropna()
-print(data.shape)
-print(list(data.columns))
-print(data.head())
+#print(data.shape)
+#print(list(data.columns))
+#print(data.head())
+
+data['education'].unique()
+#print(data['education'].unique())
+data['education']=np.where(data['education'] == 'basic.9y', 'Basic',data['education'] )
+data['education']=np.where(data['education'] == 'basic.4y', 'Basic',data['education'] )
+data['education']=np.where(data['education'] == 'basic.6y', 'Basic',data['education'] )
+#print(data['education'].unique())
+
+data['y'].value_counts()
+#print(data['y'].value_counts())
+#print(data['y'].unique())
+
+sns.countplot(x = 'y', data=data, palette='hls')
+#plt.show()
+plt.savefig('count_plot')
+
+data.groupby('y').mean()
+print(data.groupby('y').mean())
