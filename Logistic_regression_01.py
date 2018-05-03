7cf16d153c6874543ed46c4be87e01208b81320c
Haoxiang Yang <yanghaox@oregonstate.edu>
2018-05-02 20:46:31 -0700

    visually analyze datasets

--- Logistic_regression_01.py
+++ Logistic_regression_01.py
@@ -25,9 +25,63 @@ data['y'].value_counts()
 #print(data['y'].value_counts())
 #print(data['y'].unique())
 
-sns.countplot(x = 'y', data=data, palette='hls')
+#sns.countplot(x = 'y', data=data, palette='hls')
 #plt.show()
-plt.savefig('count_plot')
+#plt.savefig('count_plot')
 
 data.groupby('y').mean()
-print(data.groupby('y').mean())
+data.groupby('education').mean()
+data.groupby('marital').mean()
+data.groupby('job').mean()
+#print(data.groupby('y').mean())
+#print(data.groupby('education').mean())
+#print(data.groupby('marital').mean())
+#print(data.groupby('job').mean())
+
+'''
+#analysis the data set
+
+
+pd.crosstab(data.job, data.y,).plot(kind='bar')
+plt.title('Purchase Frequency for job title')
+plt.xlabel('Job')
+plt.ylabel('frequecy of purchase')
+plt.savefig('purchase_fre_job')
+
+pd.crosstab(data.marital, data.y).plot(kind='bar')
+plt.title('Stacked bar chart of marital status vs pruchase')
+plt.xlabel('marital status')
+plt.ylabel('proportion of customers')
+plt.savefig('marital_vs_pur_stack')
+
+table = pd.crosstab(data.education, data.y)
+table.div(table.sum(1).astype(float), axis=0).plot(kind='bar', stacked=True)
+plt.title('Stacked bar chart of educaton vs purchase')
+plt.xlabel('education')
+plt.ylabel('proportion of customers')
+plt.savefig('ed_vs_pur_stack')
+
+pd.crosstab(data.month, data.y).plot(kind='bar', stacked=False)
+plt.title('purchase frequency for month')
+plt.xlabel('month')
+plt.ylabel('frequency of purchase')
+plt.savefig('pur_fre_month_bar')
+
+
+data.age.hist()
+plt.title('Histogram of Age')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+plt.savefig('hist_age')
+
+pd.crosstab(data.poutcome,data.y).plot(kind='bar')
+plt.title('Purchase Frequency for Poutcome')
+plt.xlabel('Poutcome')
+plt.ylabel('Frequency of Purchase')
+plt.savefig('pur_fre_pout_bar')
+plt.show()
+'''
+#create dummy variables
+
+cat_vars = 
+
