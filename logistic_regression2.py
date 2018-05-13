1e8b9e3fa38d48bb040f4687589fd6ef988cc199
Haoxiang-surface <yanghaox@oregonstate.edu>
2018-05-13 12:20:46 -0700

    https://github.com/BigDataGal/Data-Mania-Demos/blob/master/Logistic%20Regression%20Demo.ipynb

---
+++ logistic_regression2.py
@@ -0,0 +1,14 @@
+'''
+the dataset from the tatannic
+
+'''
+
+
+
+import numpy as np
+import pandas as pd
+import  seaborn as sb
+import  matplotlib.pyplot as plt
+import sklearn
+
+from pandas import Series, DataFrame
