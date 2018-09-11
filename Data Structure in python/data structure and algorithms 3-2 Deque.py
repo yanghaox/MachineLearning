b30f4e8f5478472b1e2a39ee7c3d36f1e44517ed
Haoxiang Yang <yanghaox@oregonstate.edu>
2018-09-11 14:25:42 +0800

    9/10 Deque

--- Data Structure in python/data structure and algorithms 3-2 Deque.py
+++ Data Structure in python/data structure and algorithms 3-2 Deque.py
@@ -10,4 +10,29 @@ Deque
 而从后面添加和删除是 $$O(n)$$。
 考虑到添加和删除项是出现的常见操作，这是可预期的。
 同样，重要的是要确定我们知道在实现中前后都分配在哪里。
+
+
+from pythonds.basic.deque import Deque
+
+def palchecker(aString):
+	chardeque = Deque()
+
+	for ch in aString:
+		chardeque.addRear(ch)
+		print(chardeque.items)
+	stillEqual = True
+
+	while chardeque.size() > 1 and stillEqual:
+		first = chardeque.removeFront()
+		print("first", first)
+		last = chardeque.removeRear()
+		print("last", last)
+		if first != last:
+			stillEqual = False
+
+	return stillEqual
+
+print(palchecker("lkdkjfskl"))
+#print(palchecker("radar"))
+
 """
